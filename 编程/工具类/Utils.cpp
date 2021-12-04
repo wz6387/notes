0033b3856ee974008fa61d5d1480fe24f58701c1
wz6387 <465655310@qq.com>
2021-12-04 17:46:05 +0800

    提交工具类

---
+++ 编程/工具类/Utils.cpp
@@ -0,0 +1,32 @@
+﻿#include "Utils.h"
+#include <algorithm>
+
+#ifdef __WINDOWS__
+#include <Windows.h>
+#else
+#include <limits.h>
+#endif
+
+using namespace molo;
+
+size_t Utils::getExecutablePath(std::string& exepath)
+{
+#ifdef __WINDOWS__
+	char szExe[512] = { 0 };
+	GetModuleFileName(NULL, szExe, 512);
+	exepath = szExe;
+	size_t pos = exepath.find_last_of('\\');
+	exepath = exepath.substr(0, pos);
+	exepath += '\\';
+	return exepath.size();
+#else
+	char szExe[512] = { 0 };
+	if (readlink("/proc/self/exe", szExe, sizeof(szExe)) <= 0)
+		return -1;
+	exepath = szExe;
+	size_t pos = exepath.find_last_of('/');
+	exepath = exepath.substr(0, pos);
+	exepath += '/';
+	return exepath.size();
+#endif
+}
---
+++ 编程/工具类/Utils.h
@@ -0,0 +1,14 @@
+﻿#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <string>
+
+namespace molo {
+class Utils
+{
+public:
+	static size_t getExecutablePath(std::string& exepath);
+};
+}// namespace molo
+
+#endif // _UTILS_H_
