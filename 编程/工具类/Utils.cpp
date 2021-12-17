bbc05c58dc6b3aae3ac295192a3cc81caa7e387e
wz6387 <465655310@qq.com>
2021-12-17 14:44:32 +0800

    增加函数

--- 编程/工具类/Utils.cpp
+++ 编程/工具类/Utils.cpp
@@ -1,10 +1,12 @@
 ﻿#include "Utils.h"
 #include <algorithm>
+#include <sstream>
 
 #ifdef __WINDOWS__
 #include <Windows.h>
 #else
 #include <limits.h>
+#include <iconv.h>
 #endif
 
 using namespace molo;
@@ -29,4 +31,146 @@ size_t Utils::getExecutablePath(std::string& exepath)
 	exepath += '/';
 	return exepath.size();
 #endif
+}
+
+std::string Utils::gbkToUtf8(const std::string& source)
+{
+    std::string utf8Str;
+    utf8Str.resize(source.length() * 2, 0);
+#ifdef WIN32
+
+    int len = MultiByteToWideChar(CP_ACP, 0, source.c_str(), -1, NULL, 0);
+    wchar_t* wstr = new wchar_t[len + 1];
+    memset(wstr, 0, len + 1);
+    MultiByteToWideChar(CP_ACP, 0, source.c_str(), -1, wstr, len);
+    len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+    char* str = new char[len + 1];
+    memset(str, 0, len + 1);
+    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
+    std::string strTemp = str;
+    if (wstr) {
+        delete[] wstr;
+    }
+    if (str) {
+        delete[] str;
+    }
+    return strTemp;
+#else
+    iconv_t cd;
+    cd = iconv_open("UTF-8", "GBK");
+    if ((iconv_t) - 1 == cd) {
+        printf("iconv_open error");
+        return "";
+    }
+
+    char* uniCh = const_cast<char*>(source.c_str());
+    size_t uniSize = source.length();
+
+    char* utfCh = const_cast<char*>(utf8Str.c_str());
+    size_t utfSize = utf8Str.length();
+    int retsize = iconv(cd, &uniCh, &uniSize, &utfCh, &utfSize);
+    iconv_close(cd);
+    if ((size_t) - 1 == retsize) {
+        return "";
+    }
+#endif
+    return utf8Str;
+}
+
+std::string Utils::utf8ToGbk(const std::string& utf8Str)
+{
+    std::string unicodeStr;
+    unicodeStr.resize(utf8Str.length() * 2, 0);
+#ifdef WIN32
+    // Convert headers from ASCII to Unicode.
+    size_t dwMinSize = MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), utf8Str.length(), NULL, 0);
+    WCHAR* pWTmp = new WCHAR[dwMinSize + 1];
+    memset(pWTmp, 0, dwMinSize * 2 + 2);
+    MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), utf8Str.length(), pWTmp, dwMinSize);
+
+    dwMinSize = WideCharToMultiByte(CP_ACP, NULL, pWTmp, -1, NULL, 0, NULL, FALSE);
+    if (utf8Str.length() < dwMinSize) {
+        unicodeStr.resize(dwMinSize, 0);
+    }
+
+    char* tmp = const_cast<char*>(unicodeStr.c_str());
+    WideCharToMultiByte(CP_ACP, NULL, pWTmp, -1, tmp, unicodeStr.length(), NULL, FALSE);
+    delete[]pWTmp;
+    pWTmp = NULL;
+#else
+    iconv_t cd;
+    cd = iconv_open("GBK", "UTF-8");
+    if ((iconv_t) - 1 == cd) {
+        return "";
+    }
+    char* utf8Ch = const_cast<char*>(utf8Str.c_str());
+    size_t utf8Size = utf8Str.length();
+
+    char* uniCh = const_cast<char*>(unicodeStr.c_str());
+    size_t uniSize = unicodeStr.length();
+
+    int retsize = iconv(cd, &utf8Ch, &utf8Size, &uniCh, &uniSize);
+    iconv_close(cd);
+    if (retsize < 0) {
+        return "";
+    }
+#endif
+    return unicodeStr;
+}
+
+void Utils::splitString(const std::string& src, char delim, std::vector<std::string>& elems)
+{
+	std::stringstream ss(src);
+	std::string item;
+	while (std::getline(ss, item, delim))
+	{
+		if(!item.empty())
+		{
+			elems.push_back(item);
+		}
+	}
+}
+
+void Utils::splitString(const std::string& s, const char* delim, std::vector<std::string>& elems)
+{
+	if (s.empty()) {
+		return;
+	}
+    elems.clear();                                 // 1.
+    char* buffer = new char[s.size() + 1];      // 2.
+    buffer[s.size()] = '\0';
+    std::copy(s.begin(), s.end(), buffer);      // 3.
+    char* p = strtok(buffer, delim);       // 4.
+    do {
+        elems.push_back(p);                        // 5.
+    } while ((p = strtok(NULL, delim)));   // 6.
+    delete[] buffer;
+}
+
+std::string Utils::toLower(const std::string& data)
+{
+	std::string result(data);
+	for(std::string::iterator iter = result.begin(); iter != result.end(); ++iter)
+	{
+		if(*iter >= 'A' && *iter <= 'Z')
+		{
+			*iter = *iter - ('A' - 'a');
+		}
+	}
+
+	return result;
+}
+
+std::string Utils::toUpper(const std::string& data)
+{
+	std::string result(data);
+	for(std::string::iterator iter = result.begin(); iter != result.end(); ++iter)
+	{
+		if(*iter >= 'a' && *iter <= 'z')
+		{
+			*iter = *iter + ('A' - 'a');
+		}
+	}
+
+	return result;
 }
--- 编程/工具类/Utils.h
+++ 编程/工具类/Utils.h
@@ -2,12 +2,26 @@
 #define _UTILS_H_
 
 #include <string>
+#include <vector>
 
 namespace molo {
 class Utils
 {
 public:
+	//获取当前执行程序路径
 	static size_t getExecutablePath(std::string& exepath);
+	//gbk与utf8转换
+	static std::string gbkToUtf8(const std::string& utf16Str);
+    static std::string utf8ToGbk(const std::string& utf8Str);
+	
+	//字符串str使用分隔符delim进行分割，得到tokens列表
+	static void splitString(const std::string& str, char delim, std::vector<std::string>& elems);
+	static void splitString(const std::string& str, const char* delim, std::vector<std::string>& elems);
+	
+	// 字符串转大小写
+	static std::string toLower(const std::string& data);
+	static std::string toUpper(const std::string& data);
+	
 };
 }// namespace molo
 
