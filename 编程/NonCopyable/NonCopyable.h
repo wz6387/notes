ed884b4dd76768b246faf300359bfa3e86262e67
wz6387 <465655310@qq.com>
2021-12-29 14:52:26 +0800

    Create NonCopyable.h

---
+++ 编程/NonCopyable/NonCopyable.h
@@ -0,0 +1,9 @@
+﻿//如果希望类不能复制，就从这个类派生即可
+class NonCopyable
+{
+protected:
+	NonCopyable() = default;
+	~NonCopyable() = default;
+	NonCopyable(const NonCopyable&) = delete;//禁用复制构造函数
+	NonCopyable& operate = (const NonCopyable&) = delete;//禁用赋值构造函数
+};
