801f4a3fb63c18ad6e9766fb689811b42f3a8140
wz6387 <465655310@qq.com>
2022-05-10 10:21:38 +0800

    Update Utils.cpp

--- 编程/工具类/Utils.cpp
+++ 编程/工具类/Utils.cpp
@@ -147,16 +147,16 @@ void Utils::splitString(const std::string& s, const char* delim, std::vector<std
     delete[] buffer;
 }
 
-void Utils::splitString(const std::string& str, const std::string& delim, std::vector<std::string>& elems)
+void Utils::splitString(const std::string& str, const std::string& pattern, std::vector<std::string>& elems)
 {
 	std::string::size_type pos;
-	str += pattern;//扩展字符串以方便操作
-	int size = str.size();
+	std::string tmpStr = str + pattern;//扩展字符串以方便操作
+	int size = tmpStr.size();
 
 	for (int i = 0; i < size; i++) {
-		pos = str.find(pattern, i);
+		pos = tmpStr.find(pattern, i);
 		if (pos < size) {
-			std::string s = str.substr(i, pos - i);
+			std::string s = tmpStr.substr(i, pos - i);
 			elems.push_back(s);
 			i = pos + pattern.size() - 1;
 		}
